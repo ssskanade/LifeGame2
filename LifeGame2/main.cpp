056dc898adb5890cad7ea5a6c109763b53ab20f2
Kanade <kikuchi.kanade@gmail.com>
2019-04-30 10:28:40 +0900

    Initial Commit

---
+++ LifeGame2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LifeGame2
+//
+//  Created by Shu Kikuchi on 2019/04/30.
+//  Copyright © 2019 Shu Kikuchi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
